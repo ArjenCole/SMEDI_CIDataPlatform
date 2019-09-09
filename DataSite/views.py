96a8c21504b0a5c55e53cc21fc2d0fe69c7f7d19
Arjen Cole <jinhesky@hotmail.com>
2019-09-09 14:36:14 +0800

    Initial commit

---
+++ DataSite/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+
+
+def index(request):
+    # return HttpResponse("hello world!")
+    return render(request, "index.html",)
+
+
+def login(request):
+    return render(request, "login.html",)
+
+
+def register(request):
+    return render(request, "register.html",)
+
+
+def tables(request):
+    return render(request, "tables.html",)
+
+
+def charts(request):
+    return render(request, "charts.html",)
+
+
+def forms(request):
+    return render(request, "forms.html",)
+
