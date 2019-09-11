351a4ecf0e772504b622c501ea811ddcfe93732c
ArjenCole <jinhesky@hotmail.com>
2019-09-11 21:16:54 +0800

    读取excel文件时校验文件格式

--- DataSite/viewscheck.py
+++ DataSite/viewscheck.py
@@ -2,8 +2,13 @@ import xlrd
 
 
 def CheckFile(pFilePath):
-    data = xlrd.open_workbook(pFilePath)
-    table = data.sheets()[0]
-    cell_A1 = table.row(0)[1].value
-    rtList = [cell_A1, 'chemistry', 1997, 2000]
-    return rtList
+    try:
+        data = xlrd.open_workbook(pFilePath)
+    except:
+        rtList = ['文件读取失败,请确认文件格式为xls或xlsx']
+        return rtList
+    else:
+        table = data.sheets()[0]
+        cell_A1 = table.row(0)[1].value
+        rtList = [cell_A1, 'chemistry', 1997, 2000]
+        return rtList
