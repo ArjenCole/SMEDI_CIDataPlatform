53434e0eee0f4cac8395ebfb7e41a885ec581fb0
crane <jinhesky@hotmail.com>
2019-09-15 11:39:45 +0800

    gantt增加中文引用

--- static/jQueryGantt/example/data.js
+++ static/jQueryGantt/example/data.js
@@ -1,7 +1,7 @@
 var ganttData = [
 	{
-		id: 1, name: "Feature 1", series: [
-			{ name: "Planned", start: new Date(2010,1,1), end: new Date(2010,1,3), color: "#54e69d"},
+		id: 1, name: "张三", series: [
+			{ name: "上海市中环国定路下匝道", start: new Date(2010,1,1), end: new Date(2010,1,3), color: "#54e69d"},
 			{ name: "Actual", start: new Date(2010,1,2), end: new Date(2010,1,5), color: "#ffc36d" }
 		]
 	}, 
