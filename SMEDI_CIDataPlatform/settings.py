3284637d8ec08fc7bde3bdcf72f00cec9ed4847d
jinhe <jinhesky@hotmail.com>
2019-11-19 14:22:48 +0800

    建立数据库模型,生成数据库 数据库需要在premium中创建，表格由django创建 要添加pymysql引用

--- SMEDI_CIDataPlatform/settings.py
+++ SMEDI_CIDataPlatform/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import pymysql         # 一定要添加这两行！通过pip install pymysql!
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DataSite',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'SMEDI_CIDataPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # mysql数据库引擎
+        'NAME': 'db_django_cidp',  # 数据库名字
+        'USER': 'root',  # 用户名
+        'PASSWORD': '123456',  # 密码
+        'HOST': 'localhost',  # 主机
+        'PORT': '3306'  # 端口
     }
 }
 
@@ -106,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
