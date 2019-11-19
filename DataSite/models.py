84d15b750e6e4d102a65932e1d9132bea4dee6e7
jinhe <jinhesky@hotmail.com>
2019-11-19 14:46:16 +0800

    修改用户与部门的关系为多对多，自动生成中间表

--- DataSite/models.py
+++ DataSite/models.py
@@ -3,27 +3,27 @@ from django.db import models
 # Create your models here.
 
 
-class Departments(models.Model):
-    """部门"""
-    name = models.CharField(max_length=40)
-    abbreviation = models.CharField(max_length=40)
-    parent = models.CharField(max_length=40)
-    remark = models.CharField(max_length=40)
-
-    def __unicode__(self):
-        return self.name
-
-
 class Users(models.Model):
     """用户"""
     Account = models.EmailField()
     Password = models.CharField(max_length=50)
     Name = models.CharField(max_length=50)
     Email = models.EmailField()
-    Department = models.ForeignKey(Departments)
     Major = models.CharField(max_length=50)
     Remark = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.Account
+
+
+class Departments(models.Model):
+    """部门"""
+    name = models.CharField(max_length=40)
+    abbreviation = models.CharField(max_length=40)
+    parent = models.CharField(max_length=40)
+    users = models.ManyToManyField(Users)
+    remark = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return self.name
 
