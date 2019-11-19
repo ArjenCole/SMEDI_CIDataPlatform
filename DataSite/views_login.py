b09088edc5203907853e8475e7c6193515d6158a
jinhe <jinhesky@hotmail.com>
2019-11-19 15:37:46 +0800

    登录页面校验账号密码后登录

--- DataSite/views_login.py
+++ DataSite/views_login.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from DataSite.models import Users
+import hashlib
+import base64
+import time
+import datetime
+import json
 # Create your views here.
 
 
 def login(request):
-    return render(request, "login.html",)
+    if request.method == 'GET':
+        return render(request, "login.html")
+    if request.method == 'POST':
+        result = checkin(request.POST)
+        if result == "登陆失败":
+            return render(request, "login.html")
+        else:
+            return render(request, "index.html", {'data': result})
+
+
+def checkin(post):
+    account = post.get("loginUseraccount", None)
+    password = post.get("loginPassword", None)
+    a = (account + password)
+    epassword = encode(a)
+    q1 = Users.objects.filter(Account=account)
+    q2 = q1.filter(Password=epassword)
+    if q2.count() > 0:
+        return q2[0]
+    else:
+        return "登陆失败"
+
+
+def encode(s):
+    b = bytes(s, encoding='gb2312')
+    h = hashlib.sha512(b).digest()
+    c = base64.b64encode(h)
+    d = str(c, encoding='gb2312')
+    return d
