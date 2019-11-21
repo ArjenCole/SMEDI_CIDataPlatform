5c87601479eb3396fb1e68171fde83f186eaa621
ArjenCole <jinhesky@hotmail.com>
2019-11-21 23:00:57 +0800

    修改数据库模型，但暂未关联数据库

--- DataSite/models.py
+++ DataSite/models.py
@@ -16,13 +16,32 @@ class Users(models.Model):
         return self.Account
 
 
+class Roles(models.Model):
+    """角色"""
+    Name = models.CharField(max_length=40)
+    User = models.ManyToManyField(Users)
+
+    def __unicode__(self):
+        return self.Name
+
+
 class Departments(models.Model):
     """部门"""
     Name = models.CharField(max_length=40)
     Abbreviation = models.CharField(max_length=40)
     Parent = models.CharField(max_length=40, null=True)
-    Users = models.ManyToManyField(Users)
+    User = models.ManyToManyField(Users)
     Remark = models.CharField(max_length=40, null=True)
+
+    def __unicode__(self):
+        return self.Name
+
+
+class Premissions(models.Model):
+    """权限"""
+    Name = models.CharField(max_length=40)
+    Department = models.ForeignKey(Departments)
+    Role = models.ManyToManyField(Roles)
 
     def __unicode__(self):
         return self.Name
